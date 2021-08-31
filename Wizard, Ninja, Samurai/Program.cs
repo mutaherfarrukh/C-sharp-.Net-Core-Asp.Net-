e9c9f8d7aa85e1a242f0cac772485591619407af
Mutaher Farrukh <86249387+mutaherfarrukh@users.noreply.github.com>
2021-08-31 13:40:49 -0500

    Add files via upload

---
+++ Wizard, Ninja, Samurai/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Wizard__Ninja__Samurai
+{
+    class Human
+{
+    public string Name;
+    public int Strength;
+    public int Intelligence;
+    public int Dexterity;
+    private int health;
+     
+    public int Health
+    {
+        get { return health; }
+    }
+     
+    public Human(string name)
+    {
+        Name = name;
+        Strength = 3;
+        Intelligence = 3;
+        Dexterity = 3;
+        health = 100;
+    }
+     
+    public Human(string name, int str, int intel, int dex, int hp)
+    {
+        Name = name;
+        Strength = str;
+        Intelligence = intel;
+        Dexterity = dex;
+        health = hp;
+    }
+     
+    // Build Attack method
+    public int Attack(Human target)
+    {
+        int dmg = Strength * 3;
+        target.health -= dmg;
+        Console.WriteLine($"{Name} attacked {target.Name} for {dmg} damage!");
+        return target.health;
+    }
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+
+        }
+    }
+}
